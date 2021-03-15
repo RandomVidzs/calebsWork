63d44da49ef708f3c1ca908ad06e757ec2954460
Random Vidzs <calebpedicanjr@gmail.com>
2021-03-15 00:51:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+#week 3 i think 
+age = 15
+print (age)
+my_fav_class = "Keyboarding"
+
+print (my_fav_class)
+myCousinsAge = 16
+print (myCousinsAge)
+
+bothOfOurAges = (age) + (myCousinsAge)
+print (bothOfOurAges)
+#print ((str"age") + (str"myCousinsAge"))
+n1 = 2
+n2 = 4
+result = n1 + n2 
+print ("The Result of 2 and 4 is ", result) 
+
+n3 = 13
+n4 = 31 
+result = n3 + n4 
+print (result)
+
+
+result = n1 + n2 + n3 + n4 
+print (result)
+
+print (input(result))
+
+spacer + " "
+#this is camel casing 
+#myFavClass = "Coding"
+#print(myFavClass)
